0fa92ae4032fd4a54ce7fb9fa15f9b38976c82f4
bmark88 <bradley.mark@hotmail.com>
2020-01-05 20:50:08 +0000

    created function to determine if a number is odd or not

---
+++ odd.js
@@ -0,0 +1,11 @@
+function isOdd(num) {
+  if (num % 2 ==! 0) {
+    console.log("the number is odd");
+  } else {
+    console.log("the numer is even");
+  }
+}
+/*
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+*/
