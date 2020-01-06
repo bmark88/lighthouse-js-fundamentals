b9c60692a95c9878546f2a18b2fab0fc69f3afa4
bmark88 <bradley.mark@hotmail.com>
2020-01-06 19:08:53 +0000

    turned For loop into While loop with same functionality to print packingList array

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,17 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/*
+//For loop to display packingList array
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+//turned the above For loop into a While loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.legnth) {
+  console.log(packingList[i]);
+  i++
 }
